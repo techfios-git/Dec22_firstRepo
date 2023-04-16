d0d8273a9e41e0b10d3fe5d6a6083bb3e6647065
techfios-git <shihabul.kh@gmail.com>
2023-04-16 12:49:19 -0500

    committed

---
+++ src/variousConcepts/LaunchChrome.java
@@ -0,0 +1,18 @@
+package variousConcepts;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+	}
+
+}
